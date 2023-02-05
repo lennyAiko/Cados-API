4db753e726a85f266d00505e44ca14aab0992da8
Lennox Charles <lennyaiko17@gmail.com>
2023-02-05 16:48:02 +0100

    models created

--- base/views.py
+++ base/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
 
+# don't deploy with sqlite
+
+@api_view(['GET'])
 def endpoints(req):
     data = ['/advocates', 'advocates/:username']
-    return JsonResponse(data, safe=False)
+    return Response(data)
 
 def advocate_list(req):
     data = ['Dennis', 'Lennox', 'Max', 'Tadas']
