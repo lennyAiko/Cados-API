cb96416f767a8a4a440a7c90e3001c7ef9cad653
Lennox Charles <lennyaiko17@gmail.com>
2023-02-06 16:26:04 +0100

    data now serialize

--- base/views.py
+++ base/views.py
@@ -4,6 +4,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from .models import Advocate
+from .serializers import AdvocateSerializer
 
 # Create your views here.
 
@@ -14,11 +15,15 @@ def endpoints(req):
     data = ['/advocates', 'advocates/:username']
     return Response(data)
 
+@api_view(['GET'])
 def advocate_list(req):
     # data = ['Dennis', 'Lennox', 'Max', 'Tadas']
     advocates = Advocate.objects.all()
-    return JsonResponse(advocates, safe=False)
+    serializer = AdvocateSerializer(advocates, many=True)
+    return Response(serializer.data)
 
+@api_view(['GET'])
 def advocate_detail(req, username):
-    data = username
-    return JsonResponse(data, safe=False)
+    advocate = Advocate.objects.get(username=username)
+    serializer = AdvocateSerializer(advocate, many=False)
+    return Response(serializer.data)
