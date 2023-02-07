0280c3278c052c1474a0522e1cb37c730dc87ad6
Lennox Charles <lennyaiko17@gmail.com>
2023-02-07 18:12:06 +0100

    serializer updated

--- base/serializers.py
+++ base/serializers.py
@@ -1,12 +1,16 @@
 from rest_framework.serializers import ModelSerializer
 from .models import Advocate, Company
 
-class AdvocateSerializer(ModelSerializer):
-    class Meta:
-        model = Advocate
-        fields = '__all__'
-
 class CompanySerializer(ModelSerializer):
     class Meta:
         model = Company
         fields = '__all__'
+
+class AdvocateSerializer(ModelSerializer):
+    company = CompanySerializer()
+    class Meta:
+        model = Advocate
+        fields = [
+            'username', 'bio', 'company'
+        ]
+
