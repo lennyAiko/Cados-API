a6616758de2b492500b204f1b1ca178bcf75867a
Lennox Charles <lennyaiko17@gmail.com>
2023-02-09 14:35:33 +0100

    authentication works

--- base/urls.py
+++ base/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
 from . import views
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    # TokenRefreshView,
+)
+
 urlpatterns = [
     path('', views.endpoints),
     
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+
     path('advocates/', views.advocate_list, name="advocates"),
     # path('advocates/<str:username>/', views.advocate_detail)
     path('advocates/<str:username>/', views.AdvocateDetail.as_view()),
