598c6d4de978c7fb076c43f8f59c00609a0672b2
Lennox Charles <lennyaiko17@gmail.com>
2023-02-09 14:16:31 +0100

    authentication has begun

--- base/views.py
+++ base/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from django.db.models import Q
 
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
 
 from .models import Advocate, Company
 from .serializers import AdvocateSerializer, CompanySerializer 
@@ -19,6 +20,7 @@ def endpoints(req):
     return Response(data)
 
 @api_view(['GET', 'POST'])
+@permission_classes([IsAuthenticated])
 def advocate_list(req):
     # Handles GET requests
     #/advocates/?query=#
