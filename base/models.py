71dcbfc5b37c1421186bc5f9ebdb7498de41534f
Lennox Charles <lennyaiko17@gmail.com>
2023-02-11 15:37:44 +0100

    model labels added

--- base/models.py
+++ base/models.py
@@ -15,8 +15,11 @@ class Advocate(models.Model):
             on_delete=models.SET_NULL, 
             null=True, blank=True
         ) # cascade deletes the advocate
-    username = models.CharField(max_length=200)
-    bio = models.TextField(max_length=250, null=True, blank=True)
+    name = models.CharField(max_length=500, null=True, blank=True)
+    profile_pic = models.CharField(max_length=500, null=True, blank=True)
+    username = models.CharField(max_length=500, null=True, blank=True)
+    bio = models.TextField(max_length=550, null=True, blank=True)
+    twitter = models.URLField(null=True, blank=True)
 
     def __str__(self):
         return self.username
